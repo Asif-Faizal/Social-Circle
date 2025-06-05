ccc876f63106b5454f9668ca65b13a2e56a462c8
Asif-Faizal <moh.asif@protonmail.ch>
2025-06-05 15:30:18 +0530

    feature(user): implemented user details bloc

--- frontend/lib/feature/home/bloc/user_details/user_details_bloc.dart
+++ frontend/lib/feature/home/bloc/user_details/user_details_bloc.dart
@@ -1,13 +1,31 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
 
-part 'user_details_event.dart';
-part 'user_details_state.dart';
+import '../../../../core/error/failures.dart';
+import '../../domain/usecases/user.details.dart';
+import 'user_details_event.dart';
+import 'user_details_state.dart';
 
 class UserDetailsBloc extends Bloc<UserDetailsEvent, UserDetailsState> {
-  UserDetailsBloc() : super(UserDetailsInitial()) {
-    on<UserDetailsEvent>((event, emit) {
-      // TODO: implement event handler
+  final UserDetailsUsecase userDetailsUsecase;
+  UserDetailsBloc({required this.userDetailsUsecase}) : super(UserDetailsState.initial()) {
+    on<UserDetailsEvent>((event, emit) async {
+      emit(const UserDetailsState.loading());
+
+      final result = await userDetailsUsecase(
+        UserDetailsParams(deviceId: event.deviceId, accessToken: event.accessToken),
+      );
+
+      emit(
+        result.fold(
+          (failure) {
+            if (failure is NetworkFailure) {
+              return UserDetailsState.networkError(failure.message);
+            }
+            return UserDetailsState.error(failure.message);
+          },
+          (data) => UserDetailsState.success(data),
+        ),
+      );
     });
   }
 }
